4e2e88334379b95e1e09f2414b1c61badb7539ad
ravidu_senevirathne <senevirathneravidu@gmail.com>
2025-04-06 20:47:43 +0530

    created enums and database seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\PermissionsEnum;
+use App\Enums\RolesEnum;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +15,18 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $userRole = Role::create(['name'=>RolesEnum::User->value]);
+        $vendorRole = Role::create(['name'=>RolesEnum::Vendor->value]);
+        $adminRole = Role::create(['name'=>RolesEnum::Admin->value]);
+
+        $approveVendors = Permission::create(['name' => PermissionsEnum::ApproveVendors->value]);
+        $buyProducts = Permission::create(['name' => PermissionsEnum::BuyProducts->value]);
+        $sellProducts = Permission::create(['name' => PermissionsEnum::SellProducts->value]);
+
+        $userRole->syncPermissions([$buyProducts,]);
+        $vendorRole->syncPermissions([$sellProducts,$buyProducts]);
+        $adminRole->syncPermissions([$sellProducts,
+            $buyProducts,
+            $approveVendors]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\RolesEnum;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -12,6 +13,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+         User::factory()->create([
+            'name'=>'user',
+            'email'=>'user@example.com',
+        ])->assignRole(RolesEnum::User->value);
+
+         User::factory()->create([
+            'name'=>'vendor',
+            'email'=>'vendor@example.com',
+        ])->assignRole(RolesEnum::Vendor->value);
+
+         User::factory()->create([
+            'name'=>'admin',
+            'email'=>'admin@example.com',
+        ])->assignRole(RolesEnum::Admin->value);
     }
 }
